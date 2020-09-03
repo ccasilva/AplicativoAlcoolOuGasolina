e5de127734ef212b79d978913ccec2e8f466eb6b
Carlos Silva <58012018+ccasilva@users.noreply.github.com>
2020-09-02 23:24:51 -0300

    Alterei o texto para Dev

--- lib/Home.dart
+++ lib/Home.dart
@@ -49,7 +49,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Alcool ou Gasolina"),
+        title: Text("AlcoolLLL ou GasolinaAAAA"),
         backgroundColor: Colors.blue,
       ),
       body: Container(
